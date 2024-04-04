a1eb37a0eaf93857168edb63a540bd3f1f69ce3d
jankwa <halienovajana@gmail.com>
2024-04-04 13:09:13 +0200

    Update discordgamebot.py

--- discordgamebot.py
+++ discordgamebot.py
@@ -1,70 +1,86 @@
 import enum, random, sys
 from copy import deepcopy
 
-import os, discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.messages = True
-
-DISCORD_TOKEN = os.getenv("MTE5OTc3MTMyNTgyNTI4NjE3NA.Gj10YZ.V6SjkPlUyPXGShL90Gh-jE2R2rcNnuFxfn9A4E")
-bot = commands.Bot(command_prefix="*", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} has connected to Discord!")
-    
-@bot.event
-async def on_message(message):
-    if message.content == "test":
-        await message.channel.send("Testing 1 2 3")
-    await bot.process_commands(message)
-
-bot.run(DISCORD_TOKEN)
-
 # GAME MODES
 class GameMode(enum.IntEnum):
   adventure = 1
   boss = 2
   arena = 3
 
+# BASIC CLASSES
 class Entity():
   
-    def __init__(self, name, hp, max_hp, attack, defense, xp, gold):
+    def __init__(self, name, hp, maxhp, atk, defense, xp, gold):
         self.name = name
         self.hp = hp
-        self.max_hp = max_hp
-        self.attack = attack
+        self.maxhp = maxhp
+        self.attack = atk
         self.defense = defense
         self.xp = xp
         self.gold = gold
 
-    def fight(self, other):
-        defense = min(other.defense, 19) # cap defense value
-        chance_to_hit = random.randint(0, 20-defense)
-        if chance_to_hit:
+    def fight(self, other): # NEDOKONCENE
+        defense = min(other.defense, 19)
+        chancetohit = random.randint(0, 20-defense)
+        if chancetohit:
             damage = self.attack
         else:
             damage = 0
-
         other.hp -= damage
-
         return (self.attack, other.hp <= 0) # fatal dmg
 
 class Character(Entity):
    
-    def __init__(self, name, hp, max_hp, attack, defense, chakra, level, xp, gold, inventory, mode, battling, user_id):
-        super().__init__(name, hp, max_hp, attack, defense, xp, gold)
-        self.chakra = chakra
+    def __init__(self, name, hp, maxhp, atk, defense, ch, chn, level, xp, gold, inventory, mode, user_id):
+        super().__init__(name, hp, maxhp, atk, defense, xp, gold)
+        self.chakra = ch
         self.level = level
+        self.chakranature = chn
 
         self.inventory = inventory
 
         self.mode = mode
-        self.battling = battling
         self.user_id = user_id
 
 class Enemy(Entity):
 
-    def __init__(self, name, max_hp, attack, defense, xp, gold):
-        super().__init__(name, max_hp, max_hp, attack, defense, xp, gold)
+    def __init__(self, name, maxhp, atk, defense, xp, gold):
+        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+
+# ENEMY CLASSES 
+class MagicalEnemy(Enemy):
+
+    def __init__(self, name, maxhp, atk, defense, xp, gold, magic):
+        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+        self.magic = magic
+
+class PhysicalEnemy(Enemy):
+
+    def __init__(self, name, maxhp, atk, defense, xp, gold):
+        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+
+class BossEnemy(Enemy):
+
+    def __init__(self, name, maxhp, atk, defense, xp, gold, bonus):
+        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+        self.bonus = bonus
+
+# ENEMIES
+titan = PhysicalEnemy("Titan", 260, 25, 30, 100, 25)
+dragon = BossEnemy("Dragon", 590, 95, 100, 550, 120)
+dragongiant = BossEnemy("Giant Dragon", 780, 110, 115, 690, 210)
+
+# DISCORD    
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.messages = True
+DISCORD_TOKEN = ("MTE5OTc3MTMyNTgyNTI4NjE3NA.Gj10YZ.V6SjkPlUyPXGShL90Gh-jE2R2rcNnuFxfn9A4E")
+bot = commands.Bot(command_prefix="*", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to your Discord!")
+
+bot.run(DISCORD_TOKEN)

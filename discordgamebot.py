7ba04fc0902d5136d619aef295bec256fe11e37e
jankwa <halienovajana@gmail.com>
2024-04-21 17:58:03 +0200

    Update discordgamebot.py

--- discordgamebot.py
+++ discordgamebot.py
@@ -1,12 +1,6 @@
 import enum, random, sys
 from copy import deepcopy
 
-# GAME MODES
-class GameMode(enum.IntEnum):
-  adventure = 1
-  boss = 2
-  arena = 3
-
 # BASIC CLASSES
 class Entity():
   
@@ -57,30 +51,43 @@ class MagicalEnemy(Enemy):
 class PhysicalEnemy(Enemy):
 
     def __init__(self, name, maxhp, atk, defense, xp, gold):
-        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+        super().__init__(name, maxhp, atk, defense, xp, gold)
 
 class BossEnemy(Enemy):
 
     def __init__(self, name, maxhp, atk, defense, xp, gold, bonus):
-        super().__init__(name, maxhp, maxhp, atk, defense, xp, gold)
+        super().__init__(name, maxhp, atk, defense, xp, gold)
         self.bonus = bonus
 
-# ENEMIES
-titan = PhysicalEnemy("Titan", 260, 25, 30, 100, 25)
-dragon = BossEnemy("Dragon", 590, 95, 100, 550, 120)
-dragongiant = BossEnemy("Giant Dragon", 780, 110, 115, 690, 210)
 
 # DISCORD    
 import discord
 from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
 
-intents = discord.Intents.default()
-intents.messages = True
-DISCORD_TOKEN = ("MTE5OTc3MTMyNTgyNTI4NjE3NA.Gj10YZ.V6SjkPlUyPXGShL90Gh-jE2R2rcNnuFxfn9A4E")
-bot = commands.Bot(command_prefix="*", intents=intents)
+DISCORD_TOKEN = ("MTE5OTc3MTMyNTgyNTI4NjE3NA.G8qfnM.gY7c03PU4xyBG03bmfuKJd9sWG-rYh-l-GZhiQ")
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} has connected to your Discord!")
+    print("Bot is ready!")
+
+# .MINE
+@bot.command(aliases = ["m", "mn"])
+@commands.cooldown(1, 30, commands.BucketType.user)
+async def mine(ctx):
+
+    mine_materials = ["stone", "rock", "diorite", "dirt", "iron", "coal", "copper", "granite", "chalk"]
+    rare_mine_materials = ["diamond", "gold"]
+
+    mine_luck = [discord.Embed(description = f"{ctx.author.mention} mined in a cave for so long and found a *{random.choice(rare_mine_materials)}*!", color = 1752220), discord.Embed(description = f"{ctx.author.mention} mined in a cave for a while and found *{random.choice(mine_materials)}.*", color = 3426654), discord.Embed(description = f"{ctx.author.mention} mined in a cave for few minutes and returned with *{random.choice(mine_materials)}* and *{random.choice(mine_materials)}*.", color = 3426654)]
+
+    mine = random.choice(mine_luck)
+    await ctx.send(embed = mine)
+@mine.error
+async def mine(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        em = discord.Embed(title = f"You are too tired to mine again!", description = f"Try again in {error.retry_after:.2f}s.", color = 10038562)
+        await ctx.send(embed = em)
 
 bot.run(DISCORD_TOKEN)
